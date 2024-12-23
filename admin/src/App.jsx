ba19deebf270194435be72b42cd28dbde245a55b
Benjamin Okumu <benjaminokumu12@gmail.com>
2024-12-23 19:32:43 +0300

    admin updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import OTPVerification from "./pages/OTPVerification";
+import StartPage from "./pages/StartPage";
+import useStore from "./store/index";
+import Dashboard from "./pages/Dashboard";
+import Analytics from "./pages/Analytics";
+import Followers from "./pages/Followers";
+import Contents from "./pages/Content";
+import WritePost from "./pages/WritePost";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Layout() {
+  const { user } = useStore((state) => state);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const location = useLocation();
+
+  return user?.token ? (
+    <div className='w-full h-screen'>
+      <Navbar />
+      <div className='w-full h-full flex border-t pt-16 '>
+        <div className='hidden lg:flex '>
+          <Sidebar />
+        </div>
+
+        <div className='w-full flex-1 px-8 py-4 overflow-y-auto'>
+          <Outlet />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  ) : (
+    <Navigate to='/auth' state={{ from: location }} replace />
+  );
 }
 
-export default App
+function App() {
+  return (
+    <main className='w-full min-h-screen'>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index psth='/' element={<Navigate to='/dashboard' />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/analytics' element={<Analytics />} />
+          <Route path='/followers' element={<Followers />} />
+          <Route path='/contents' element={<Contents />} />
+          <Route path='/write/:postId?' element={<WritePost />} />
+        </Route>
+
+        <Route path='/auth' element={<StartPage />} />
+        <Route path='/otp-verification' element={<OTPVerification />} />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "@mantine/tiptap/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <MantineProvider>
+          <App />
+        </MantineProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
